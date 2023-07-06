946dbdb377bbb19ad3aa2eb4da0e0c55bbc1c3d6
Davim187 <daviimorais39@gmail.com>
2023-07-05 21:03:43 -0300

    .

--- src/MenuInicial/menu.js
+++ src/MenuInicial/menu.js
@@ -6,8 +6,23 @@ function menu(){
   loadSprite('bloco', 'sprites/bloco.png');
   loadSprite('tijolos', 'sprites/tijolos.png');
   loadSprite('bg', 'sprites/background.png');
-  loadSprite('mario', 'sprites/mario.png');
+  // loadSprite('mario', 'sprites/mario.png');
   loadSprite('cano', 'sprites/cano.png');
+
+  loadSprite('mario', 'sprites/marioAndando.png',{
+    sliceX:3.9,
+    anims:{
+      idle:{
+        from:0,
+        to:0
+      },
+      move:{
+        from:1,
+        to:2
+      }
+    }
+  });
+
   
   scene('game', () => {
     add([sprite('bg') ,pos(0, -21)]);
@@ -22,7 +37,7 @@ function menu(){
     '2                                2',
     '2                                2',
     '2                                2',
-    '2               22               2',
+    '2              222               2',
     '2                     22         2',
     '2         22          22     2   2',
     '2    2    2           22   22  3 2',
@@ -37,24 +52,58 @@ function menu(){
     3: () =>[sprite('cano'), solid(), area()]
   };
   const player = add([
-    sprite("mario"),
-    pos(70,0),
+        
+    sprite("mario", {
+        animSpeed: 1,
+        frame: 0
+    }),
+    solid(),
     area(),
     body(),
-  ])
+    pos(60,0),
+    origin('bot'),
+    {
+        speed: 120
+    }
+])
   onKeyDown('left',()=>{
-    player.move(-150, 0)
+    player.move(-120, 0)
     player.flipX(true)
   })
+
   onKeyDown('right',()=>{
-    player.move(150, 0)
+    player.move(120, 0)
     player.flipX(false)
   })
+
   onKeyPress('space',()=>{
     if(player.isGrounded()){
       player.jump(440)
     }
   })
+
+
+  keyPress('left', () => {
+    player.flipX(true)
+    player.play('move')
+})
+
+keyPress('right', () => {
+    player.flipX(false)
+    player.play('move')
+})    
+
+//////////////////////
+
+// animar parado //
+keyRelease('left', () => {
+    player.play('idle')
+})
+
+keyRelease('right', () => {
+    player.play('idle')
+})
+
   const gameLevel = addLevel(maps, levelCfg);
 })
 go('game');
