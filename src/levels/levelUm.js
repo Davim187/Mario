23a3b24291eada963743d020133d25edf4e47a3d
Davim187 <daviimorais39@gmail.com>
2023-07-07 13:23:20 -0300

    TelaMenu

--- src/levels/levelUm.js
+++ src/levels/levelUm.js
@@ -3,16 +3,9 @@ function levelUm() {
   loadSprite('tijolos', 'sprites/tijolos.png');
   loadSprite('bg', 'sprites/background.png');
   loadSprite('cano', 'sprites/cano.png');
-  loadSprite('cogumeloE', 'sprites/cogumeloE.png', {
-    anims: {
-      idle: {
-        from: 0,
-        to: 0,
-        loop: true,
-      },
-    },
-  });
+  loadSprite('cogumeloE', 'sprites/cogumeloE.png');
   loadSprite('cogumeloD', 'sprites/cogumeloD.png');
+  loadSprite('flor', 'sprites/flor.png');
   loadSprite('mario', 'sprites/marioAndando.png', {
     sliceX: 3.9,
     anims: {
@@ -29,27 +22,61 @@ function levelUm() {
     },
   });
 
-  function addButton(txt, p, f) {
-    const btn = add([
-      text(txt, { size: 28 }),
-      pos(p),
+  let direction = 1;
+  let direction1 = 1;
+  let speed = 25;
+
+  function moverCogumelos() {
+    function invertDirectionE() {
+      direction1 *= -1;
+    }
+    function invertDirection() {
+      direction *= -1;
+    }
+
+    var cogumelo = add([
+      sprite('cogumeloE'),
       area(),
-      scale(1),
-      origin('center'),
+      solid(),
+      pos(250, 300),
+      origin('bot'),
+      'cogumelo',
     ]);
-
-    btn.onClick(f);
-
-    btn.onUpdate(() => {
-      if (btn.isHovering()) {
-        btn.scale = vec2(1.2);
-      } else {
-        btn.scale = vec2(1);
-        btn.color = rgb();
-      }
+    cogumelo.action(() => {
+      cogumelo.move(speed * direction, 0);
     });
 
-    return btn;
+    cogumelo.collides('cano', () => {
+      invertDirection();
+    });
+    cogumelo.collides('tijolos', () => {
+      invertDirection();
+    });
+    cogumelo.collides('cogumelo1', () => {
+      invertDirectionE();
+    });
+
+    var cogumelo2 = add([
+      sprite('cogumeloE'),
+      area(),
+      solid(),
+      pos(250, 300),
+      origin('bot'),
+      'cogumelo1',
+    ]);
+    cogumelo2.action(() => {
+      cogumelo2.move(-speed * direction1, 0);
+    });
+
+    cogumelo2.collides('cano', () => {
+      invertDirectionE();
+    });
+    cogumelo2.collides('tijolos', () => {
+      invertDirectionE();
+    });
+    cogumelo2.collides('cogumelo', () => {
+      invertDirection();
+    });
   }
 
   scene('game', () => {
@@ -62,7 +89,7 @@ function levelUm() {
       '2                                2',
       '2                                2',
       '2                                2',
-      '2                                2',
+      '2              4                 2',
       '2           2222                 2',
       '2                                2',
       '2     222                        2',
@@ -72,7 +99,6 @@ function levelUm() {
       '2    2                           2',
       '1111111111111111111111111111111111',
       '1111111111111111111111111111111111',
-      '1111111111111111111111111114111111',
     ];
 
     const levelCfg = {
@@ -81,29 +107,9 @@ function levelUm() {
       1: () => [sprite('bloco'), solid(), area()],
       2: () => [sprite('tijolos'), solid(), area(), 'tijolos'],
       3: () => [sprite('cano'), solid(), area(), 'cano'],
-      4: () => [sprite('cogumeloE'), solid(), area(), 'cogumelo'],
+      4: () => [sprite('flor'), solid(), area(), 'flor'],
     };
-
-    const cogumeloE = add([
-      sprite('cogumeloE'),
-      solid(),
-      area(),
-      body(),
-      pos(250, 300),
-      origin('bot'),
-    ]);
-
-    action('cogumelo', () => {
-      cogumeloE.move(-20, 0);
-      // cogumeloE.flipX(false);
-      cogumeloE.collides('tijolos', () => {
-        cogumeloE.play('idle');
-        cogumeloE.flipX(true);
-        if (cogumeloE.flipX(true)) {
-          cogumeloE.move(20, 0);
-        }
-      });
-    });
+    moverCogumelos();
     const player = add([
       sprite('mario', {
         animSpeed: 1,
@@ -117,6 +123,11 @@ function levelUm() {
     ]);
 
     player.onCollide('cano', () => {
+      keyPress('down', () => {
+        go('game');
+      });
+    });
+    player.onCollide('cogumelo', () => {
       keyPress('down', () => {
         go('game');
       });
