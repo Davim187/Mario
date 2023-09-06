91831fb5b1620c7d408c9c582862b8d10ca28d84
davim187 <daviimorais39@gmail.com>
2023-09-06 15:06:14 -0300

    ajutes

--- src/main.js
+++ src/main.js
@@ -2,7 +2,5 @@ import kaboom from 'kaboom';
 
 kaboom({ scale: 2, background: [0, 0, 0, 1] });
 
-// import menu from './MenuInicial/menu';
-// menu();
-import levelSeis from './levels/levelCinco';
-levelSeis();
+import menu from './MenuInicial/menu';
+menu();
